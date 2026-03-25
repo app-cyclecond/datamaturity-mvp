b63a2bb364934b634649bd5b72df7ef85288f30e
DataMaturity Dev <dev@datamaturity.com>
2026-03-25 10:06:12 -0400

    Feat: Integrate Sidebar component in Configurações page

--- app/assessment/page.tsx
+++ app/assessment/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Sidebar } from "@/components/layout/sidebar";
 
 import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
--- app/biblioteca/page.tsx
+++ app/biblioteca/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Sidebar } from "@/components/layout/sidebar";
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
